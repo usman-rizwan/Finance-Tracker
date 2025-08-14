22992fea8e2b33dc6c9b7ac85f048e3d2b39b3fb
Usman Rizwan <usmanrizwan771@gmail.com>
2025-08-14 17:27:46 +0500

    Add ensureMonthlyBalance utility to WalletService
    Introduces the ensureMonthlyBalance function to WalletService for creating or retrieving a MonthlyBalance record for a user and wallet for a given month. Also updates the Prisma schema to rename the unique constraint for monthly balances.

--- src/services/WalletService.ts
+++ src/services/WalletService.ts
@@ -1,3 +1,4 @@
+'use server'
 import { db } from "~/server/db";
 
 import type { TransactionType } from "@prisma/client";
@@ -34,5 +35,67 @@ const createWallet = async (userId: string, data: any) => {
     } catch (error) {
         console.error('Error creating wallet->', error);
         throw new Error('Erro creating wallet')
+    }
+}
+
+const ensureMonthlyBalance = async (prismaTx: typeof db, userId: string, walletId: string, year: number, month: number) => {
+
+    try {
+        const userWalletByMonth = { userId, walletId, year, month } as any;
+
+        const existingBalacne = await prismaTx.monthlyBalance.findUnique({
+            where: {
+                monthly_balance_per_wallet: userWalletByMonth
+            },
+        });
+
+        if (existingBalacne) return existingBalacne;
+
+        const previousMonth = month === 1 ? 12 : month - 1;
+        const previousYear = month === 1 ? year - 1 : year;
+
+        // get previocus month balance
+        const previousBalance = await prismaTx.monthlyBalance.findUnique({
+            where: {
+                monthly_balance_per_wallet: {
+                    userId,
+                    walletId,
+                    year: previousYear,
+                    month: previousMonth,
+                },
+            }
+        })
+
+        let openingBalance = '0';
+        if (previousBalance) {
+            openingBalance = previousBalance.closingBalance.toString();
+        } else {
+            const wallet = await prismaTx.wallet.findUnique({
+                where: {
+                    id: walletId
+                }
+            });
+
+            if (wallet) openingBalance = wallet.balance.toString()
+        }
+
+
+        const newBalance = await prismaTx.monthlyBalance.create({
+            data: {
+                userId,
+                walletId,
+                year,
+                month,
+                openingBalance,
+                totalIncome: "0",
+                totalExpense: "0",
+                closingBalance: openingBalance,
+            },
+        })
+            
+        return newBalance;
+    } catch (error: any) {
+        console.log('Error creating/fetching monthly balance:', error);
+        throw new Error('Error creating/fetching monthly balance:', error)
     }
 }
