2bb95b843704ed79aaa88cadd39db215023d7be2
Usman Rizwan <usmanrizwan771@gmail.com>
2025-09-24 23:21:21 +0500

    Update WalletService.ts

--- src/services/WalletService.ts
+++ src/services/WalletService.ts
@@ -7,39 +7,41 @@ const getYearMonth = (date: Date) => {
     return { year: date.getFullYear(), month: date.getMonth() + 1 }
 }
 
+// function to get overall )all wallets summary for a given month and year
 
 
 
 
 
-const getMonthlySummary = async (userId: string, year: number, month: number, walletId?: string) => {
-    const query: any = { userId, year, month };
-    if (walletId) query.wallet = walletId;
 
-    const balances = await db.monthlyBalance.findMany({ where: query, include: { wallet: true } });
+    const getMonthlySummary = async (userId: string, year: number, month: number, walletId?: string) => {
+        const query: any = { userId, year, month };
+        if (walletId) query.wallet = walletId;
 
-    const summary = balances.reduce(
-        (acc, balance) => {
-            acc.opening = acc.opening + Number(balance.openingBalance.toString());
-            acc.totalIncome = acc.totalIncome + Number(balance.totalIncome.toString());
-            acc.totalExpense = acc.totalExpense + Number(balance.totalExpense.toString());
-            acc.closing = acc.closing + Number(balance.closingBalance.toString());
-            return acc;
-        },
-        { opening: 0, totalIncome: 0, totalExpense: 0, closing: 0 }
-    );
+        const balances = await db.monthlyBalance.findMany({ where: query, include: { wallet: true } });
 
-    return {
-        year,
-        month,
-        summary,
+        const summary = balances.reduce(
+            (acc, balance) => {
+                acc.opening = acc.opening + Number(balance.openingBalance.toString());
+                acc.totalIncome = acc.totalIncome + Number(balance.totalIncome.toString());
+                acc.totalExpense = acc.totalExpense + Number(balance.totalExpense.toString());
+                acc.closing = acc.closing + Number(balance.closingBalance.toString());
+                return acc;
+            },
+            { opening: 0, totalIncome: 0, totalExpense: 0, closing: 0 }
+        );
 
+        return {
+            year,
+            month,
+            summary,
+
+        }
     }
-}
 
 
-export {
-    getYearMonth,
-    getMonthlySummary
-};
+    export {
+        getYearMonth,
+        getMonthlySummary
+    };
 
